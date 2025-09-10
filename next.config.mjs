d66e8b60b416dc574e40bb61909ebab7c6c04fae
Miles Le <ZkuYb7UhAN4D@protonmail.com>
2025-09-09 21:01:17 -0700

    domain fix

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
   output: 'export', // Move this line outside the object
   trailingSlash: true,
   // other configurations...
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bradygomez.com/' : '',
 };
 
 // Export the configuration object
